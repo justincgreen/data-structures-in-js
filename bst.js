0f4b73b335bd3f8082867f66dedcf26a8a19856f
Justin Green <justingreendev@gmail.com>
2024-09-23 20:49:49 -0500

    Binary Search Tree

--- bst.js
+++ bst.js
@@ -11,3 +11,78 @@ class Node{
     this.right = null;
   }
 }
+
+// We create a class for the BST
+class BinarySearchTree {
+  // The tree has only one property which is its root node
+  constructor(){
+      this.root = null
+  }
+  // The insert method takes a value as parameter and inserts the value in its corresponding place within the tree
+  insert(value){
+    const newNode = new Node(value)
+    if(this.root === null){
+        this.root = newNode
+        return this
+    }
+    let current = this.root
+    while(true){
+        if(value === current.value) return undefined
+        if(value < current.value){
+            if(current.left === null){
+                current.left = newNode
+                return this
+            }
+            current = current.left
+        } else {
+            if(current.right === null){
+                current.right = newNode
+                return this
+            } 
+            current = current.right
+        }
+    }
+  }
+  
+  // The find method takes a value as parameter and iterates through the tree looking for that value
+  // If the value is found, it returns the corresponding node and if it's not, it returns undefined
+  find(value){
+    if(this.root === null) return false
+    let current = this.root,
+        found = false
+    while(current && !found){
+        if(value < current.value){
+            current = current.left
+        } else if(value > current.value){
+            current = current.right
+        } else {
+            found = true
+        }
+    }
+    if(!found) return undefined
+    return current
+  }
+
+// The contains method takes a value as parameter and returns true if the value is found within the tree
+  contains(value){
+    if(this.root === null) return false
+    let current = this.root,
+        found = false
+    while(current && !found){
+        if(value < current.value){
+            current = current.left
+        } else if(value > current.value){
+            current = current.right
+        } else {
+            return true
+        }
+    }
+    return false
+  }
+}
+
+const myTree = new BinarySearchTree;
+myTree.insert('value1');
+myTree.insert('value2');
+myTree.insert('value3');
+console.log(myTree);
